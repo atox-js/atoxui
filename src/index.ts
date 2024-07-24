d69a2039a6b959ba8aabfdeae15979413d212790
atox-js <atom.022509@gmail.com>
2024-07-24 10:15:15 +0900

    update exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import { Avatar } from "./components/Avatar";
+import { Button } from "./components/Button";
+import Input from "./components/Input";
+import Progress from "./components/Progress";
+import useToast from "./components/Toast";
+import Toast from "./components/Toast/Toast";
 import "./index.css";
-// Button
-export * from "./components/Button";
-// Toast
-export { default as useToast } from "./components/Toast";
-export { default as ToastContainer } from "./components/Toast/ToastContainer";
+
+export { Avatar, Button, Input, Progress, useToast, Toast };
