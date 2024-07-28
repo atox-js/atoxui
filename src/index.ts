8f7e5448fe5a2fcc47dff585e4fdf29f144e345f
atox-js <atom.022509@gmail.com>
2024-07-28 16:49:48 +0900

    bug fix

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,30 @@
-import "./index.css";
 import Avatar from "./components/Avatar";
 import Button from "./components/Button";
 import Input from "./components/Input";
-import Progress from "./components/Progress";
-import useToast from "./components/Toast";
-import Toast from "./components/Toast/Toast";
-import ToastContainer from "./components/Toast/ToastContainer";
 import Modal from "./components/Modal/Modal";
 import ModalBody from "./components/Modal/ModalBody";
 import ModalFooter from "./components/Modal/ModalFooter";
 import ModalHeader from "./components/Modal/ModalHeader";
+import Progress from "./components/Progress";
+import Spinner from "./components/Spinner";
+import Switch from "./components/Switch";
+import useToast from "./components/Toast";
+import Toast from "./components/Toast/Toast";
+import ToastContainer from "./components/Toast/ToastContainer";
+import "./index.css";
 
 export {
   Avatar,
   Button,
   Input,
-  Progress,
-  useToast,
-  Toast,
-  ToastContainer,
   Modal,
-  ModalHeader,
   ModalBody,
   ModalFooter,
+  ModalHeader,
+  Progress,
+  Spinner,
+  Switch,
+  Toast,
+  ToastContainer,
+  useToast,
 };
